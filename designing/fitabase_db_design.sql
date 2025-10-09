8db8067be0a4b6cf14aea3fc5d31bb8bde6b8cdc
Danijia <hdanijia@gmail.com>
2025-10-08 22:58:26 -0400

    10/08/25: Adding in progress project files

---
+++ designing/fitabase_db_design.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "daily_activity" (
+    "id" TEXT,
+    "activity_date" TEXT,
+    "total_steps" INTEGER, 
+    "total_distance" REAL,
+    "tracker_distance" REAL, 
+    "logged_activities" INTEGER,
+    "very_active_distance" REAL, 
+    "moderately_active_distance" REAL,
+    "light_active_distance" REAL, 
+    "sedentary_active_distance" REAL,
+    "very_active_minutes" INTEGER,
+    "fairly_active_minutes" INTEGER,
+    "lightly_active_minutes" INTEGER, 
+    "sedentary_minutes" INTEGER,
+    "calories" INTEGER,
+)
+  
